3a850efc5175d0bb2cc0516011524748eac518a0
benoitmagnier13-ux <benoitmagnier13@gmail.com>
2026-05-13 16:50:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 const express = require('express')
-const path = require('path')
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'public'), {
-  setHeaders: (res, filePath) => {
-    if (filePath.endsWith('.html')) {
-      res.setHeader('Content-Type', 'text/html; charset=utf-8')
-    }
-  }
-}))
+app.use(express.static('public'))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  res.sendFile('index.html', { root: 'public' })
 })
 
 app.listen(process.env.PORT || 3000)
